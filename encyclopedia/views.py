c99683c04aa2a3e12888d998abe63ce02e00ce38
Lucas Lima <limalucas7027@gmail.com>
2023-10-02 20:51:46 -0300

    Created function checkEntry. This function verifies if an entry exists, converts markdown to html and returns it. Started to implement search input that returns if an entry exists.

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django import forms
 import markdown2
 from . import util
@@ -6,26 +6,30 @@ from . import util
 def index(request):
     if request.method == "POST":
         searched = request.POST['q']
-        return render(request, "encyclopedia/search.html", {
-            "searched": searched
-        })
-    
+        if checkEntry(searched) != 'Requested page was not found':
+            return redirect("/" + searched)
+
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
 def showEntry(request, title):
+    html = checkEntry(title)
+    return render(request, "encyclopedia/showEntry.html", {
+        "html": html
+        })
+    
+def checkEntry(title):
     markdownContent = util.get_entry(title)
-
     if markdownContent != None:
         html = markdown2.markdown(markdownContent)
-        return render(request, "encyclopedia/showEntry.html", {
-        "html": html
-        })
+        return html
     else:
         html = "Requested page was not found"
-        return render(request, "encyclopedia/showEntry.html", {
-        "html": html
-        })
+        return html
+
+
+
+    
 
 
